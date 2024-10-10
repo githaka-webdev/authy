cd8e3171bf5162852fcc289b260643716bab963f
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 12:54:15 +0300

    feat(registry): registered the Brand component

--- app/components/utils/components.ts
+++ app/components/utils/components.ts
@@ -2,3 +2,4 @@ export { default as Header } from '../layout/Header';
 export { default as Main } from '../layout/Main';
 export { default as Footer } from '../layout/Footer';
 export { default as Container } from '../layout/Container';
+export { default as Brand } from '../ui/Brand';
