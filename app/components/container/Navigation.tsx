b0a82771e402f2fb1850a4567b0681c264875182
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 15:16:48 +0300

    feat(container): Navigation - mounted navigation menu

--- app/components/container/Navigation.tsx
+++ app/components/container/Navigation.tsx
@@ -1,5 +1,11 @@
 import { NavigationStyles } from '@/styles';
+import { NavigationMenu } from '@/components';
 
 export default function Navigation({ children }: NavigationProps) {
-    return <nav className={NavigationStyles.navigation}>{children}</nav>;
+    return (
+        <nav className={NavigationStyles.navigation}>
+            {children}
+            <NavigationMenu>{children}</NavigationMenu>
+        </nav>
+    );
 }
