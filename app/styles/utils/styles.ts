404125b8e604db1ab583430c3a1cd0c7fb903414
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 12:22:12 +0300

    style: created layout styles and updated the styles registry

--- app/styles/utils/styles.ts
+++ app/styles/utils/styles.ts
@@ -1,1 +1,4 @@
-
+export { default as HeaderStyles } from '../layout/Header.module.css';
+export { default as MainStyles } from '../layout/Main.module.css';
+export { default as FooterStyles } from '../layout/Footer.module.css';
+export { default as ContainerStyles } from '../layout/Container.module.css';
