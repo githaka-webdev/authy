8e1a1c9405002f9c4c84b9fa86fc17e5228e2494
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 19:50:15 +0300

    feat(page): Hero - updated Hero contents

--- app/components/page/Hero.tsx
+++ app/components/page/Hero.tsx
@@ -1,3 +1,26 @@
+import Image from 'next/image';
+
 export default function Hero() {
-    return <section></section>;
+    return (
+        <section>
+            <div>
+                <Image
+                    src={'/images/landing/hero.jpg'}
+                    alt='Hero Image'
+                    width={900}
+                    height={600}
+                />
+            </div>
+            <div>
+                <h1>Lorem ipsum dolor </h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Nam, eos corporis incidunt nemo blanditiis, a quia
+                    dignissimos minus hic possimus aliquam praesentium
+                    laudantium libero. Exercitationem recusandae asperiores
+                    dolorem incidunt quidem!
+                </p>
+            </div>
+        </section>
+    );
 }
