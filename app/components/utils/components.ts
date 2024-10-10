fcd5f114ca3996bcbfb2229d22a7572a370fe788
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 13:11:43 +0300

    feat(registry): updated Navigation component

--- app/components/utils/components.ts
+++ app/components/utils/components.ts
@@ -3,3 +3,4 @@ export { default as Main } from '../layout/Main';
 export { default as Footer } from '../layout/Footer';
 export { default as Container } from '../layout/Container';
 export { default as Brand } from '../ui/Brand';
+export { default as Navigation } from '../container/Navigation';
