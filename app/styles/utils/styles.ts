7ca6ea786c389f626f7f20736e38981048794a57
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 13:16:09 +0300

    style(registry): updated navigation component styles

--- app/styles/utils/styles.ts
+++ app/styles/utils/styles.ts
@@ -3,3 +3,4 @@ export { default as MainStyles } from '../layout/Main.module.css';
 export { default as FooterStyles } from '../layout/Footer.module.css';
 export { default as ContainerStyles } from '../layout/Container.module.css';
 export { default as BrandStyles } from '../ui/Brand.module.css';
+export { default as NavigationStyles } from '../container/Navigation.module.css';
