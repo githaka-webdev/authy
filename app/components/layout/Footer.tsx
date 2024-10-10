37592344ddd0c464c52bd667107e90d4344e38da
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 17:41:23 +0300

    feat(layout): Footer - updated content

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,5 +1,30 @@
+import { Brand, NavigationItem } from '@/components';
 import { FooterStyles } from '@/styles';
 
 export default function Footer() {
-    return <footer className={FooterStyles.footer}></footer>;
+    return (
+        <footer className={FooterStyles.footer}>
+            <div>
+                <Brand />
+            </div>
+            <div>
+                <h1>Resources</h1>
+                <NavigationItem
+                    link=''
+                    name='Docs'
+                />
+                <NavigationItem
+                    link=''
+                    name='Learn'
+                />
+                <NavigationItem
+                    link=''
+                    name='Downloads'
+                />
+            </div>
+            <div>
+                <p>&copy;2024, Authy. All rights reserved.</p>
+            </div>
+        </footer>
+    );
 }
