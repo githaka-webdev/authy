bbbcb89d7c1a7d23bcdaf8f2404c8a4a27d854d1
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 12:58:35 +0300

    feat(ui): Brand - updated Brand component contents

--- app/components/ui/Brand.tsx
+++ app/components/ui/Brand.tsx
@@ -1,3 +1,14 @@
+import Image from 'next/image';
+
 export default function Brand() {
-    return <div></div>;
+    return (
+        <div>
+            <Image
+                src={'./logo.svg'}
+                alt='Authy Logo'
+                width={35}
+                height={35}
+            />
+        </div>
+    );
 }
