39e5ef2e27d7373a45f47b3a45d52d4ee2b2c05a
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 12:09:31 +0300

    feat: components registry - registered all current layout components

---
+++ app/components/utils/components.ts
@@ -0,0 +1,4 @@
+export { default as Header } from '../layout/Header';
+export { default as Main } from '../layout/Main';
+export { default as Footer } from '../layout/Footer';
+export { default as Container } from '../layout/Container';
