8d5163f8bfd385aadafd6ae5bd76342cd5439aa3
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 18:09:04 +0300

    style(layout): Footer - updated FooterStyles

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -4,23 +4,36 @@ import { FooterStyles } from '@/styles';
 export default function Footer() {
     return (
         <footer className={FooterStyles.footer}>
-            <div>
-                <Brand />
-            </div>
-            <div>
-                <h1>Resources</h1>
-                <NavigationItem
-                    link=''
-                    name='Docs'
-                />
-                <NavigationItem
-                    link=''
-                    name='Learn'
-                />
-                <NavigationItem
-                    link=''
-                    name='Downloads'
-                />
+            <div className={FooterStyles.footer_navigation}>
+                <div className={FooterStyles.footer_brand}>
+                    <Brand />
+                </div>
+                <div className={FooterStyles.footer_links}>
+                    <h1>Resources</h1>
+                    <NavigationItem
+                        link=''
+                        name='Docs'
+                    />
+                    <NavigationItem
+                        link=''
+                        name='Learn'
+                    />
+                    <NavigationItem
+                        link=''
+                        name='Downloads'
+                    />
+                </div>
+                <div className={FooterStyles.footer_links}>
+                    <h1>More</h1>
+                    <NavigationItem
+                        link=''
+                        name='Cookies'
+                    />
+                    <NavigationItem
+                        link=''
+                        name='Privacy'
+                    />
+                </div>
             </div>
             <div>
                 <p>&copy;2024, Authy. All rights reserved.</p>
