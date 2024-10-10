f13a0bfa1def4ded3b10d9b51f787a0e0b5a28e9
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 12:56:35 +0300

    style(registry): updated the Brand css module

--- app/styles/utils/styles.ts
+++ app/styles/utils/styles.ts
@@ -2,3 +2,4 @@ export { default as HeaderStyles } from '../layout/Header.module.css';
 export { default as MainStyles } from '../layout/Main.module.css';
 export { default as FooterStyles } from '../layout/Footer.module.css';
 export { default as ContainerStyles } from '../layout/Container.module.css';
+export { default as BrandStyles } from '../ui/Brand.module.css';
