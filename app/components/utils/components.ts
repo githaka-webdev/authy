56bf5e530fd0ffed1514ef8ef90916ae774ab867
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 13:25:29 +0300

    feat(registry): registered NavigationItem component

--- app/components/utils/components.ts
+++ app/components/utils/components.ts
@@ -4,3 +4,4 @@ export { default as Footer } from '../layout/Footer';
 export { default as Container } from '../layout/Container';
 export { default as Brand } from '../ui/Brand';
 export { default as Navigation } from '../container/Navigation';
+export { default as NavigationItem } from '../ui/NavigationItem';
