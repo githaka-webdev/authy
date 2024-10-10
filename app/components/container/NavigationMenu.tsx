9a5dde5499ce67c554e3507a824dc88dc86e750b
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 15:49:48 +0300

    feat(container): NavigationMenu - added more content & logic

--- app/components/container/NavigationMenu.tsx
+++ app/components/container/NavigationMenu.tsx
@@ -1,10 +1,33 @@
+'use client';
+import { useState } from 'react';
 import { NavigationMenuStyles } from '@/styles';
+import Image from 'next/image';
 
 export default function NavigationMenu({ children }: NavigationMenuProps) {
+    const [menu, openMenu] = useState(false);
+    function toggleMenu() {
+        openMenu(!menu);
+    }
     return (
         <main className={NavigationMenuStyles.navigation_menu}>
-            <section></section>
-            <section>{children}</section>
+            <section onClick={toggleMenu}>
+                {menu ? (
+                    <Image
+                        src={'./icons/close.svg'}
+                        alt='Close Icon'
+                        width={25}
+                        height={25}
+                    />
+                ) : (
+                    <Image
+                        src={'./icons/menu.svg'}
+                        alt='Menu Icon'
+                        width={25}
+                        height={25}
+                    />
+                )}
+            </section>
+            {menu ? <section>{children}</section> : <></>}
         </main>
     );
 }
