b692cf3ed28c5bcb5c498b02d880bb7007542db3
githaka-webdev <githaka.webdev@gmail.com>
2024-10-10 13:34:47 +0300

    feat(utils): Button - registered the Button component

--- app/components/utils/components.ts
+++ app/components/utils/components.ts
@@ -5,3 +5,4 @@ export { default as Container } from '../layout/Container';
 export { default as Brand } from '../ui/Brand';
 export { default as Navigation } from '../container/Navigation';
 export { default as NavigationItem } from '../ui/NavigationItem';
+export { default as Button } from '../container/Button';
