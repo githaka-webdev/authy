8676231190bb338e716ed205f8a58716a52614de
githaka-webdev <githaka.webdev@gmail.com>
2024-10-11 11:50:47 +0300

    feat(page): Feaures - updated Features content

--- app/components/page/Features.tsx
+++ app/components/page/Features.tsx
@@ -1,26 +1,44 @@
-import { NavigationItem } from '@/components';
+'use client';
+import { useState } from 'react';
 import { FeaturesStyles } from '@/styles';
 
 export default function Features() {
+    const [selectedFeature, setSelectedFeature] = useState('Authentication');
     return (
         <section id={FeaturesStyles.features}>
             <h1>Features</h1>
             <div className={FeaturesStyles.features_container}>
                 <div className={FeaturesStyles.features_nav}>
-                    <a href=''>Authentication</a>
-                    <a href=''>Authorization</a>
+                    <p onClick={() => setSelectedFeature('Authentication')}>
+                        Authentication
+                    </p>
+                    <p onClick={() => setSelectedFeature('Authorization')}>
+                        Authorization
+                    </p>
                 </div>
                 <div className={FeaturesStyles.features_content}>
-                    <div className={FeaturesStyles.feature_single}>
-                        <h1>Authentication</h1>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. A, vero. Reiciendis repellat maxime amet,
-                            labore nihil exercitationem dolores cupiditate
-                            impedit laudantium quibusdam, velit tempore!
-                            Incidunt qui provident repellat atque vel?
-                        </p>
-                    </div>
+                    {selectedFeature === 'Authentication' && (
+                        <div>
+                            <h2>Authentication</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipisicing elit. A, vero. Reiciendis repellat
+                                maxime amet, labore nihil exercitationem dolores
+                                cupiditate impedit laudantium. Authentication
+                            </p>
+                        </div>
+                    )}
+                    {selectedFeature === 'Authorization' && (
+                        <div>
+                            <h2>Authorization</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipisicing elit. A, vero. Reiciendis repellat
+                                maxime amet, labore nihil exercitationem dolores
+                                cupiditate impedit laudantium. Authorization.
+                            </p>
+                        </div>
+                    )}
                 </div>
             </div>
         </section>
