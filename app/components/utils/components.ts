7f6f2d3c1bb64cd3cd7d786334998a76d604e87b
githaka-webdev <githaka.webdev@gmail.com>
2024-10-11 18:02:12 +0300

    feat(utils): updated component registry with Authentication component

--- app/components/utils/components.ts
+++ app/components/utils/components.ts
@@ -1,6 +1,7 @@
 export { default as Header } from '../layout/Header';
 export { default as Main } from '../layout/Main';
 export { default as Footer } from '../layout/Footer';
+export { default as Authentication } from '../layout/Authentication';
 export { default as Container } from '../layout/Container';
 export { default as Brand } from '../ui/Brand';
 export { default as Navigation } from '../container/Navigation';
