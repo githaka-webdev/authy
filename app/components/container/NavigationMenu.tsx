ced923d692d9a13835a92173db9276e08d0c38e7
githaka-webdev <githaka.webdev@gmail.com>
2024-10-11 15:50:34 +0300

    feat(container): NavigationMenu - updated NavigationMenu content

--- app/components/container/NavigationMenu.tsx
+++ app/components/container/NavigationMenu.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import { NavigationMenuStyles } from '@/styles';
 import Image from 'next/image';
+import { Button } from '@/components';
 
 export default function NavigationMenu({ children }: NavigationMenuProps) {
     const [menu, openMenu] = useState(false);
@@ -33,6 +34,27 @@ export default function NavigationMenu({ children }: NavigationMenuProps) {
             {menu ? (
                 <section className={NavigationMenuStyles.navigation_menu_body}>
                     {children}
+                    <div>
+                        <Image
+                            src={
+                                '/images/global/navigation-menu-advert-star.png'
+                            }
+                            alt='Navigation Menu Advert Star Image'
+                            width={300}
+                            height={300}
+                        />
+                        <div>
+                            <h1>Enjoying Authy?</h1>
+                            <p>
+                                Please give it a star on GitHub to show your
+                                support!
+                            </p>
+                            <Button
+                                name='Rate Now'
+                                link=''
+                            />
+                        </div>
+                    </div>
                 </section>
             ) : (
                 <></>
