48c0fa08454aaa412bd46347e9852ec4eec927bd
githaka-webdev <githaka.webdev@gmail.com>
2024-10-11 10:55:24 +0300

    style(page): Features - updated FeaturesStyles

--- app/components/page/Features.tsx
+++ app/components/page/Features.tsx
@@ -1,30 +1,26 @@
 import { NavigationItem } from '@/components';
+import { FeaturesStyles } from '@/styles';
 
 export default function Features() {
     return (
-        <section>
+        <section id={FeaturesStyles.features}>
             <h1>Features</h1>
-            <div>
-                <NavigationItem
-                    name='Authentication'
-                    link=''
-                    active
-                />
-                <NavigationItem
-                    name='Authorization'
-                    link=''
-                />
-            </div>
-            <div>
-                <div>
-                    <h1>Authentication</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        A, vero. Reiciendis repellat maxime amet, labore nihil
-                        exercitationem dolores cupiditate impedit laudantium
-                        quibusdam, velit tempore! Incidunt qui provident
-                        repellat atque vel?
-                    </p>
+            <div className={FeaturesStyles.features_container}>
+                <div className={FeaturesStyles.features_nav}>
+                    <a href=''>Authentication</a>
+                    <a href=''>Authorization</a>
+                </div>
+                <div className={FeaturesStyles.features_content}>
+                    <div className={FeaturesStyles.feature_single}>
+                        <h1>Authentication</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. A, vero. Reiciendis repellat maxime amet,
+                            labore nihil exercitationem dolores cupiditate
+                            impedit laudantium quibusdam, velit tempore!
+                            Incidunt qui provident repellat atque vel?
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
