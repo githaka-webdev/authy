12ac1b971e164489a38a25cf92d03d1eee4ee276
githaka-webdev <githaka.webdev@gmail.com>
2024-10-11 10:28:25 +0300

    feat(page): Featueres - updated Features content

--- app/components/page/Features.tsx
+++ app/components/page/Features.tsx
@@ -1,3 +1,32 @@
+import { NavigationItem } from '@/components';
+
 export default function Features() {
-    return <div></div>;
+    return (
+        <section>
+            <h1>Features</h1>
+            <div>
+                <NavigationItem
+                    name='Authentication'
+                    link=''
+                    active
+                />
+                <NavigationItem
+                    name='Authorization'
+                    link=''
+                />
+            </div>
+            <div>
+                <div>
+                    <h1>Authentication</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A, vero. Reiciendis repellat maxime amet, labore nihil
+                        exercitationem dolores cupiditate impedit laudantium
+                        quibusdam, velit tempore! Incidunt qui provident
+                        repellat atque vel?
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 }
