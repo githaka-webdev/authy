a2ce9710859f0255c03c5627d6e6443e26745fb4
githaka-webdev <githaka.webdev@gmail.com>
2024-10-12 10:16:41 +0300

    feat(container): Form - updated Form content with styles

--- app/components/container/Form.tsx
+++ app/components/container/Form.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image';
 import { Input } from '@/components';
+import { FormStyles } from '@/styles';
 
 export default function Form({ children, type }: FormProps) {
     return (
-        <form action=''>
+        <form
+            action=''
+            className={FormStyles.form}
+        >
             <Image
                 src={'/logo.svg'}
                 alt='Authy Logo'
