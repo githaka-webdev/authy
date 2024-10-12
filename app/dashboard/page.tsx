1837d6388e5589ddafe89e721194292bf28d82ff
githaka-webdev <githaka.webdev@gmail.com>
2024-10-12 12:24:44 +0300

    feat(layout): DashboardContainer - created DashboardContainer

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,56 @@
 import type { Metadata } from 'next';
+import {
+    Authentication,
+    Brand,
+    Button,
+    Container,
+    Features,
+    Footer,
+    Form,
+    Header,
+    Hero,
+    Input,
+    Main,
+    Navigation,
+    NavigationItem,
+} from '@/components';
 
 export const metadata: Metadata = {
     title: 'Authy - Dashboard',
 };
 
 export default function Dashboard() {
-    return <div id='dashboard'></div>;
+    return (
+        <div id='dashboard'>
+            <Header>
+                <Brand />
+                <Navigation>
+                    <NavigationItem
+                        name='Landing'
+                        link='/'
+                        active
+                    />
+                    <NavigationItem
+                        name='Docs'
+                        link='/'
+                    />
+                    <NavigationItem
+                        name='Changelog'
+                        link='/'
+                    />
+                    <Button
+                        name='Account'
+                        link='/login'
+                    />
+                </Navigation>
+            </Header>
+            <Container>
+                <Main>
+                    <Hero />
+                    <Features />
+                </Main>
+                <Footer />
+            </Container>
+        </div>
+    );
 }
