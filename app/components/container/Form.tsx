7ff9e718e7bdbed7a6009e4e1ccc51c4b5e22990
githaka-webdev <githaka.webdev@gmail.com>
2024-10-12 09:44:15 +0300

    feat(container): Form - updated Form content by adding a logo and submit button

--- app/components/container/Form.tsx
+++ app/components/container/Form.tsx
@@ -1,3 +1,20 @@
+import Image from 'next/image';
+import { Input } from '@/components';
+
 export default function Form({ children }: FormProps) {
-    return <form action=''>{children}</form>;
+    return (
+        <form action=''>
+            <Image
+                src={'/logo.svg'}
+                alt='Authy Logo'
+                width={35}
+                height={35}
+            />
+            {children}
+            <Input
+                type='submit'
+                value=''
+            />
+        </form>
+    );
 }
