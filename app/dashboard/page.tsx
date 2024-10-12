5c777d04c47e2bbe4852d71687e7b65060dfb9c6
githaka-webdev <githaka.webdev@gmail.com>
2024-10-12 12:04:00 +0300

    feat(dashboard): updated metadata and content

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Authy - Dashboard',
+};
+
+export default function Dashboard() {
+    return <div id='dashboard'></div>;
+}
