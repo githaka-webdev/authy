2c7563a4f1904afa53c7d90eae85c5271d6313da
githaka-webdev <githaka.webdev@gmail.com>
2024-10-12 16:10:52 +0300

    feat(container): Dropdown - updated Dropdown props

--- app/components/container/Dropdown.tsx
+++ app/components/container/Dropdown.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function Dropdown({ link, name }: DropdownProps) {
+export default function Dropdown({
+    link,
+    name,
+    icon,
+    icon_name,
+}: DropdownProps) {
     return (
         <Link href={link}>
             <Image
-                src={}
-                alt={}
+                src={icon}
+                alt={icon_name}
                 width={30}
                 height={30}
             />
