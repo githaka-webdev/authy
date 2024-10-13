878cc25581aa842e12c6f6b718913251f095a11d
githaka-webdev <githaka.webdev@gmail.com>
2024-10-13 16:45:29 +0300

    feat(container): Dropdown
    - updated dropdown logic

--- app/components/container/Dropdown.tsx
+++ app/components/container/Dropdown.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from 'react';
 import Image from 'next/image';
 import { DropdownStyles } from '@/styles';
 
@@ -7,9 +9,16 @@ export default function Dropdown({
     icon_name,
     children,
 }: DropdownProps) {
+    const [menu, setMenu] = useState(true);
+    function toggleMenu() {
+        setMenu(!menu);
+    }
     return (
         <div className={DropdownStyles.dropdown}>
-            <div className={DropdownStyles.dropdown_title}>
+            <div
+                className={DropdownStyles.dropdown_title}
+                onClick={toggleMenu}
+            >
                 <Image
                     src={icon}
                     alt={icon_name}
@@ -25,10 +34,14 @@ export default function Dropdown({
                     className={DropdownStyles.dropdown_title_arrow_down}
                 />
             </div>
-            <div className={DropdownStyles.dropdown_body}>
-                <p>{name}</p>
-                {children}
-            </div>
+            {!menu ? (
+                <div className={DropdownStyles.dropdown_body}>
+                    <p>{name}</p>
+                    {children}
+                </div>
+            ) : (
+                <></>
+            )}
         </div>
     );
 }
