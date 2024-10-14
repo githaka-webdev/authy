4b5b9ad43c44c07de700dc0d2e11c5e860d1a5d5
githaka-webdev <githaka.webdev@gmail.com>
2024-10-14 17:30:24 +0300

    feat(components): [global] - Navigation - updated menu features

--- app/components/global/Navigation.tsx
+++ app/components/global/Navigation.tsx
@@ -1,11 +1,18 @@
+'use client';
 import { ReactNode } from 'react';
 import { Button } from '@/components';
+import Image from 'next/image';
+import { useState } from 'react';
 
 interface NavigationProps {
     children: ReactNode;
 }
 
 export default function Navigation({ children }: NavigationProps) {
+    const [menu, setMenu] = useState(true);
+    function toggleMenu() {
+        return setMenu(!menu);
+    }
     return (
         <div className='grow h-full flex flex-row items-center justify-end gap-8'>
             <nav className='hidden sm:flex sm:flex-row sm:items-center sm:justify-end sm:gap-8'>
@@ -15,15 +22,38 @@ export default function Navigation({ children }: NavigationProps) {
                     name='Account'
                 />
             </nav>
-            <nav className='sm:hidden'>
-                <div></div>
-                <div>
-                    {children}
-                    <Button
-                        link=''
-                        name='Account'
-                    />
+            <nav className='sm:hidden flex-grow flex flex-row items-center justify-end h-full'>
+                <div
+                    className='w-full flex flex-row items-center justify-end h-full hover:cursor-pointer'
+                    onClick={toggleMenu}
+                >
+                    {menu ? (
+                        <Image
+                            src={'/icons/menu.svg'}
+                            alt='Menu Icon'
+                            width={20}
+                            height={20}
+                        />
+                    ) : (
+                        <Image
+                            src={'/icons/close.svg'}
+                            alt='Close Icon'
+                            width={20}
+                            height={20}
+                        />
+                    )}
                 </div>
+                {menu ? (
+                    <></>
+                ) : (
+                    <div className='absolute top-16 flex flex-col right-4 w-2/4 p-4 bottom-0 gap-6 border-l'>
+                        {children}
+                        <Button
+                            link=''
+                            name='Account'
+                        />
+                    </div>
+                )}
             </nav>
         </div>
     );
