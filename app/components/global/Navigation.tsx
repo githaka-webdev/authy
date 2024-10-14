f16e314eea97e7f478f8b16ca8e6204b44a70dc9
githaka-webdev <githaka.webdev@gmail.com>
2024-10-14 16:02:40 +0300

    style(components): [global] - Navigation - mounted the Navigation component in the Header component - styled the Navgigation component

--- app/components/global/Navigation.tsx
+++ app/components/global/Navigation.tsx
@@ -1,3 +1,5 @@
 export default function Navigation() {
-    return <nav></nav>;
+    return (
+        <nav className='grow h-full flex flex-row items-center justify-end'></nav>
+    );
 }
--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,9 +1,11 @@
+import { Navigation } from '@/components';
 import Brand from '../global/Brand';
 
 export default function Header() {
     return (
         <header className='flex flex-row items-center justify-start gap-16 h-16 px-4 w-full sm:px-8 xl:px-16 max-w-screen-2xl'>
             <Brand name='Authy' />
+            <Navigation />
         </header>
     );
 }
