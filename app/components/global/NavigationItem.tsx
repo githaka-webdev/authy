31df3a1f6198b9b56016d91302a891187f79c62b
githaka-webdev <githaka.webdev@gmail.com>
2024-10-14 16:40:58 +0300

    feat(components): [global] - NavigationItem - added styles and conditional styles for active item

--- app/components/global/NavigationItem.tsx
+++ app/components/global/NavigationItem.tsx
@@ -3,8 +3,23 @@ import Link from 'next/link';
 interface NavigationItemProps {
     link: string;
     name: string;
+    active?: boolean;
 }
 
-export default function NavigationItem({ link, name }: NavigationItemProps) {
-    return <Link href={link}>{name}</Link>;
+export default function NavigationItem({
+    link,
+    name,
+    active,
+}: NavigationItemProps) {
+    return (
+        <Link
+            className={[
+                'text-black p-2 border-transparent border hover:border-black hover:bg-gray-100 rounded-xl',
+                [active ? 'font-semibold' : ''],
+            ].join(' ')}
+            href={link}
+        >
+            {name}
+        </Link>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Landing() {
                 <NavigationItem
                     link='/'
                     name='Landing'
+                    active
                 />
                 <NavigationItem
                     link='/'
