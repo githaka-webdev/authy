5304e12ba3cfa79dbfbcdbc64d816edf3d10aaa8
githaka-webdev <githaka.webdev@gmail.com>
2024-10-14 18:34:24 +0300

    feat: updated light and darkmode favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+'use client';
 import localFont from 'next/font/local';
 import './globals.css';
+import { useState, useEffect } from 'react';
 
 const workSansNormal = localFont({
     src: './fonts/work-sans-normal.ttf',
@@ -17,8 +19,27 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const [darkMode, isDarkMode] = useState(false);
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        isDarkMode(mediaQuery.matches);
+        const handleChange = (e: MediaQueryListEvent) => {
+            isDarkMode(e.matches);
+        };
+        mediaQuery.addEventListener('change', handleChange);
+        return () => {
+            mediaQuery.removeEventListener('change', handleChange);
+        };
+    }, []);
     return (
         <html lang='en'>
+            <head>
+                <link
+                    rel='shortcut icon'
+                    href={darkMode ? '/logo-light.svg' : '/logo-dark.svg'}
+                    type='image/x-icon'
+                />
+            </head>
             <body
                 className={`${workSansNormal.variable} ${
                     workSansItalic.variable
