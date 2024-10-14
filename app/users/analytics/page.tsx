4b086857669ce80c4d13b281cf00ae01b5216f37
githaka-webdev <githaka.webdev@gmail.com>
2024-10-14 08:47:51 +0300

    feat(users): Users
    - duplicated the dashboard to make users

---
+++ app/users/analytics/page.tsx
@@ -0,0 +1,81 @@
+import type { Metadata } from 'next';
+import {
+    Aside,
+    Authentication,
+    Brand,
+    Breadcrumbs,
+    Button,
+    Container,
+    DashboardContainer,
+    Dropdown,
+    Features,
+    Footer,
+    Form,
+    Header,
+    Hero,
+    Input,
+    Main,
+    Navigation,
+    NavigationItem,
+} from '@/components';
+
+import { MainStyles } from '@/styles';
+
+export const metadata: Metadata = {
+    title: 'Authy - Dashboard',
+};
+
+export default function Dashboard() {
+    return (
+        <div className='dashboard'>
+            <Header>
+                <Brand />
+                <Navigation>
+                    <NavigationItem
+                        name='Notifications'
+                        link='/'
+                    />
+                    <NavigationItem
+                        name='Settings'
+                        link='/'
+                    />
+                    <NavigationItem
+                        name='Account'
+                        link='/'
+                    />
+                    <Button
+                        name='Logout'
+                        link='/login'
+                    />
+                </Navigation>
+            </Header>
+            <DashboardContainer>
+                <Aside>
+                    <Dropdown
+                        link=''
+                        name='Dashboard'
+                        icon='/icons/dashboard.svg'
+                        icon_name='Dashboard Icon'
+                    >
+                        <NavigationItem
+                            name='Dashboard'
+                            link='/dashboard'
+                        />
+                        <NavigationItem
+                            name='Analytics'
+                            link='/dashboard/analytics'
+                            active
+                        />
+                        <NavigationItem
+                            name='Threats'
+                            link='/dashboard/threats'
+                        />
+                    </Dropdown>
+                </Aside>
+                <Main style={MainStyles.main_more}>
+                    <Breadcrumbs name='/Dashboard / Analytics' />
+                </Main>
+            </DashboardContainer>
+        </div>
+    );
+}
