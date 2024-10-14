85adcc77b781f1bd34fcac578d5b97f922721442
githaka-webdev <githaka.webdev@gmail.com>
2024-10-14 16:12:35 +0300

    feat(components): [layout] - Header - updated the component with props

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,13 +1,16 @@
 import { Navigation } from '@/components';
 import Brand from '../global/Brand';
+import { ReactNode } from 'react';
 
-export default function Header() {
+interface HeaderProps {
+    children: ReactNode;
+}
+
+export default function Header({ children }: HeaderProps) {
     return (
         <header className='flex flex-row items-center justify-start gap-16 h-16 px-4 w-full sm:px-8 xl:px-16 max-w-screen-2xl'>
             <Brand name='Authy' />
-            <Navigation>
-                <></>
-            </Navigation>
+            <Navigation>{children}</Navigation>
         </header>
     );
 }
