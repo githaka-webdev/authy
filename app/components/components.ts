3fc8fbf535a65294f1c375b28bbefbccfc246dd9
githaka-webdev <githaka.webdev@gmail.com>
2024-10-14 15:57:14 +0300

    feat: registered the Navigation component to be globally accessible

--- app/components/components.ts
+++ app/components/components.ts
@@ -1,3 +1,4 @@
 export { default as Logo } from './global/Logo';
 export { default as Brand } from './global/Brand';
 export { default as Header } from './layout/Header';
+export { default as Navigation } from './global/Navigation';
